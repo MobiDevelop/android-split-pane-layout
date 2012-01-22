c3a59ec9ec576a59bfd59e079e66d9dcd4da36aa
Justin Shapcott <support@mobidevelop.com>
2012-01-22 14:57:47 -0700

    Bringing up to speed with current project.

--- split-pane-layout/gen/com/mobidevelop/widget/R.java
+++ split-pane-layout/gen/com/mobidevelop/widget/R.java
@@ -9,15 +9,144 @@ package com.mobidevelop.widget;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>horizontal</code></td><td>0</td><td></td></tr>
+<tr><td><code>vertical</code></td><td>1</td><td></td></tr>
+</table>
+         */
+        public static int orientation=0x7f010000;
+        /** <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+         */
+        public static int splitterBackground=0x7f010003;
+        /** <p>May be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>May be a fractional value, which is a floating point number appended with either % or %p, such as "<code>14.5%</code>".
+The % suffix always means a percentage of the base size; the optional %p suffix provides a size relative to
+some parent container.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int splitterPosition=0x7f010002;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int splitterSize=0x7f010001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static int horizontal=0x7f040000;
+        public static int vertical=0x7f040001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static int app_name=0x7f050001;
+        public static int hello=0x7f050000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a SplitPaneLayout.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #SplitPaneLayout_orientation com.mobidevelop.widget:orientation}</code></td><td></td></tr>
+           <tr><td><code>{@link #SplitPaneLayout_splitterBackground com.mobidevelop.widget:splitterBackground}</code></td><td></td></tr>
+           <tr><td><code>{@link #SplitPaneLayout_splitterPosition com.mobidevelop.widget:splitterPosition}</code></td><td></td></tr>
+           <tr><td><code>{@link #SplitPaneLayout_splitterSize com.mobidevelop.widget:splitterSize}</code></td><td></td></tr>
+           </table>
+           @see #SplitPaneLayout_orientation
+           @see #SplitPaneLayout_splitterBackground
+           @see #SplitPaneLayout_splitterPosition
+           @see #SplitPaneLayout_splitterSize
+         */
+        public static final int[] SplitPaneLayout = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.mobidevelop.widget.R.attr#orientation}
+          attribute's value can be found in the {@link #SplitPaneLayout} array.
+
+
+          <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>horizontal</code></td><td>0</td><td></td></tr>
+<tr><td><code>vertical</code></td><td>1</td><td></td></tr>
+</table>
+          @attr name android:orientation
+        */
+        public static final int SplitPaneLayout_orientation = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.mobidevelop.widget.R.attr#splitterBackground}
+          attribute's value can be found in the {@link #SplitPaneLayout} array.
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+          @attr name android:splitterBackground
+        */
+        public static final int SplitPaneLayout_splitterBackground = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.mobidevelop.widget.R.attr#splitterPosition}
+          attribute's value can be found in the {@link #SplitPaneLayout} array.
+
+
+          <p>May be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>May be a fractional value, which is a floating point number appended with either % or %p, such as "<code>14.5%</code>".
+The % suffix always means a percentage of the base size; the optional %p suffix provides a size relative to
+some parent container.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:splitterPosition
+        */
+        public static final int SplitPaneLayout_splitterPosition = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.mobidevelop.widget.R.attr#splitterSize}
+          attribute's value can be found in the {@link #SplitPaneLayout} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:splitterSize
+        */
+        public static final int SplitPaneLayout_splitterSize = 1;
+    };
 }
--- split-pane-layout/src/com/mobidevelop/widget/SplitPaneLayout.java
+++ split-pane-layout/src/com/mobidevelop/widget/SplitPaneLayout.java
@@ -19,6 +19,394 @@
 
 package com.mobidevelop.widget;
 
-public class SplitPaneLayout {
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class SplitPaneLayout extends ViewGroup {
+	
+	private int     mOrientation;
+	private int     mSplitterSize;
+	private int     mSplitterPositionPixel;
+	private int     mSplitterPositionPercent;
+	
+	private int     mSplitterBackgroundResource;
+	
+	private boolean mSplitterPositionConfigured;
+	
+	private boolean isDragging = false;
+	private Rect temp = new Rect();
+	private Paint paint = new Paint();
+	
+	public SplitPaneLayout(Context context) {
+		super(context);
+	}
+	
+	public SplitPaneLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+	
+	public SplitPaneLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		if (attrs != null) {
+			TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SplitPaneLayout);
+			mOrientation = a.getInt(R.styleable.SplitPaneLayout_orientation, 0);
+			mSplitterSize = a.getDimensionPixelSize(R.styleable.SplitPaneLayout_splitterSize, 12);
+			
+			TypedValue value = a.peekValue(R.styleable.SplitPaneLayout_splitterPosition);
+			if (value != null) {
+				if (value.type == TypedValue.TYPE_DIMENSION) {
+					mSplitterPositionPixel = a.getDimensionPixelSize(R.styleable.SplitPaneLayout_splitterPosition, 0);
+				}
+				else if (value.type == TypedValue.TYPE_FRACTION) {
+					mSplitterPositionPercent = Math.round(a.getFraction(R.styleable.SplitPaneLayout_splitterPosition, 100, 100, 50));
+				}
+			}
+			else {
+				mSplitterPositionPixel = 0;
+				mSplitterPositionPercent = 50;				
+			}
+			mSplitterBackgroundResource = a.getResourceId(R.styleable.SplitPaneLayout_splitterBackground, 0);
+			a.recycle();
+		}
+		
+		View view = new View(getContext());
+		view.setBackgroundResource(mSplitterBackgroundResource);
+		this.addView(view);
+		view .setOnTouchListener
+		(
+			new OnTouchListener()
+			{
+				int lastX;
+				int lastY;
+				
+				@Override
+				public boolean onTouch(View v, MotionEvent event)
+				{
+					int x = (int) event.getX();
+					int y = (int) event.getY();
+					
+					switch (event.getAction())
+					{
+						case MotionEvent.ACTION_DOWN:
+						{
+							isDragging = true;
+							v.getDrawingRect(temp);
+							temp.offset(v.getLeft(), v.getTop());
+							lastX = x;
+							lastY = y;
+							invalidate();
+							break;
+						}
+						case MotionEvent.ACTION_MOVE:
+						{
+							if (isDragging)
+							{
+								switch (mOrientation)
+								{
+									case 0:
+									{
+										temp.offset((x - lastX), 0);
+										break;
+									}
+									case 1:
+									{
+										temp.offset(0, (int)(y - lastY));
+										break;
+									}								
+								}
+								lastX = x;
+								lastY = y;
+								invalidate();
+							}
+							break;
+						}
+						case MotionEvent.ACTION_UP:
+						{
+							isDragging = false;
+							switch (mOrientation)
+							{
+								case 0:
+								{
+									mSplitterPositionPixel += event.getX() - mSplitterSize / 2;
+									break;
+								}
+								case 1:
+								{
+									mSplitterPositionPixel += event.getY() - mSplitterSize / 2;
+									break;
+								}								
+							}							
+							mSplitterPositionPercent = 0;
+							requestLayout();
+							break;
+						}							
+					}
+					return true;
+				}
+				
+			}
+		);
+		
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		
+		switch(mOrientation)
+		{
+			case 0:
+			{
+				if (!mSplitterPositionConfigured)
+				{
+					if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
+					{
+						mSplitterPositionPixel = Math.round(widthSize * (mSplitterPositionPercent / 100.0f));
+						mSplitterPositionConfigured = true;
+					}
+					else
+					if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
+					{
+						mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) widthSize) * 100);
+						mSplitterPositionConfigured = true;
+					}
+					this.getChildAt(1).measure
+					(
+						MeasureSpec.makeMeasureSpec
+						(
+							mSplitterPositionPixel - (mSplitterSize / 2),
+							MeasureSpec.EXACTLY
+						)
+						,
+						MeasureSpec.makeMeasureSpec
+						(
+							heightSize,
+							MeasureSpec.EXACTLY
+						)
+					);
+					this.getChildAt(0).measure
+					(
+						MeasureSpec.makeMeasureSpec
+						(
+							mSplitterSize,
+							MeasureSpec.EXACTLY
+						)
+						,
+						MeasureSpec.makeMeasureSpec
+						(
+							heightSize,
+							MeasureSpec.EXACTLY
+						)
+					);					
+					this.getChildAt(2).measure
+					(
+						MeasureSpec.makeMeasureSpec
+						(
+							widthSize - mSplitterPositionPixel - (mSplitterSize / 2),
+							MeasureSpec.EXACTLY
+						)
+						,
+						MeasureSpec.makeMeasureSpec
+						(
+							heightSize,
+							MeasureSpec.EXACTLY
+						)
+					);					
+				}
+			}
+			case 1:
+			{
+				if (!mSplitterPositionConfigured)
+				{
+					if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
+					{
+						mSplitterPositionPixel = Math.round(heightSize * (mSplitterPositionPercent / 100.0f));
+						mSplitterPositionConfigured = true;
+					}
+					else
+					if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
+					{
+						mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) heightSize) * 100);
+						mSplitterPositionConfigured = true;
+					}
+
+					this.getChildAt(1).measure
+					(
+						MeasureSpec.makeMeasureSpec
+						(
+							widthSize,
+							MeasureSpec.EXACTLY
+						)
+						,
+						MeasureSpec.makeMeasureSpec
+						(
+
+							mSplitterPositionPixel - (mSplitterSize / 2),
+							MeasureSpec.EXACTLY							
+						)
+					);
+					this.getChildAt(0).measure
+					(
+						MeasureSpec.makeMeasureSpec
+						(
+							widthSize,
+							MeasureSpec.EXACTLY
+						)
+						,
+						MeasureSpec.makeMeasureSpec
+						(
+							mSplitterSize,
+							MeasureSpec.EXACTLY
+						)
+					);					
+					this.getChildAt(2).measure
+					(
+						MeasureSpec.makeMeasureSpec
+						(
+							heightSize,
+							MeasureSpec.EXACTLY
+						)
+						,
+						MeasureSpec.makeMeasureSpec
+						(
+							heightSize - mSplitterPositionPixel - (mSplitterSize / 2),
+							MeasureSpec.EXACTLY							
+						)
+					);					
+				}
+			}
+		}		
+	}
+	
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		int w = r - l;
+		int h = b - t;
+		
+		switch(mOrientation)
+		{
+			case 0:
+			{
+				if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
+				{
+					mSplitterPositionPixel = Math.round(w * (mSplitterPositionPercent / 100.0f));
+					mSplitterPositionConfigured = true;
+				}
+				else
+				if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
+				{
+					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) w) * 100);
+					mSplitterPositionConfigured = true;
+				}
+				else
+				{
+					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) w) * 100);
+					mSplitterPositionConfigured = true;
+				}
+				// We need to tell the child elements about their new dimensions;
+				this.getChildAt(1).measure
+				(
+					MeasureSpec.makeMeasureSpec
+					(
+						mSplitterPositionPixel - (mSplitterSize / 2) - l,
+						MeasureSpec.EXACTLY
+					)
+					,
+					MeasureSpec.makeMeasureSpec
+					(
+
+						h,
+						MeasureSpec.EXACTLY							
+					)
+				);					
+				this.getChildAt(1).layout
+				(
+					l, t, mSplitterPositionPixel - (mSplitterSize / 2), b
+				);
+				this.getChildAt(0).layout
+				(
+					mSplitterPositionPixel - (mSplitterSize / 2), t, mSplitterPositionPixel + (mSplitterSize / 2), b
+				);
+				// We need to tell the child elements about their new dimensions;
+				this.getChildAt(2).measure
+				(
+					MeasureSpec.makeMeasureSpec
+					(
+						r - (mSplitterPositionPixel + (mSplitterSize / 2)),
+						MeasureSpec.EXACTLY
+					)
+					,
+					MeasureSpec.makeMeasureSpec
+					(
+
+						h,
+						MeasureSpec.EXACTLY							
+					)
+				);						
+				this.getChildAt(2).layout
+				(
+					mSplitterPositionPixel + (mSplitterSize / 2), t, r, b
+				);		
+				break;
+			}
+			case 1:
+			{
+				if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
+				{
+					mSplitterPositionPixel = Math.round(h * (mSplitterPositionPercent / 100.0f));
+					mSplitterPositionConfigured = true;
+				}
+				else
+				if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
+				{
+					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) h) * 100);
+					mSplitterPositionConfigured = true;
+				}
+				else
+				{
+					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) h) * 100);
+					mSplitterPositionConfigured = true;
+				}
+				
+				this.getChildAt(1).layout
+				(
+					l, t, r, mSplitterPositionPixel - (mSplitterSize / 2)
+				);
+				this.getChildAt(0).layout
+				(
+					l, mSplitterPositionPixel - (mSplitterSize / 2), r, mSplitterPositionPixel + (mSplitterSize / 2)
+				);					
+				this.getChildAt(2).layout
+				(
+					l, mSplitterPositionPixel + (mSplitterSize / 2), r, b
+				);						
+				break;
+			}
+		}
+	}
+
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		super.dispatchDraw(canvas);
+		if (isDragging)
+		{
+			paint.setColor(Color.argb(128, 255,255,255));
+			canvas.drawRect(temp, paint);			
+		}		
+	}
 
 }

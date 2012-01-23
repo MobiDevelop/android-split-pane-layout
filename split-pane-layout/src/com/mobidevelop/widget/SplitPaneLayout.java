3815628fb512bb129db928cd40511cd4eb78b069
Justin Shapcott <support@mobidevelop.com>
2012-01-23 14:04:44 -0700

    Removed extra view added for Splitter. Reformatted.

--- split-pane-layout/src/com/mobidevelop/widget/SplitPaneLayout.java
+++ split-pane-layout/src/com/mobidevelop/widget/SplitPaneLayout.java
@@ -25,6 +25,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
@@ -33,132 +34,66 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class SplitPaneLayout extends ViewGroup {
-	
-	private int     mOrientation;
-	private int     mSplitterSize;
-	private int     mSplitterPositionPixel;
-	private int     mSplitterPositionPercent;
-	
-	private int     mSplitterBackgroundResource;
-	
+	private int mOrientation;
+	private int mSplitterSize;
+	private int mSplitterPositionPixel;
+	private int mSplitterPositionPercent;
+
+	private int mSplitterBackgroundResource;
+
 	private boolean mSplitterPositionConfigured;
-	
+
 	private boolean isDragging = false;
+	private Rect curr = new Rect();
 	private Rect temp = new Rect();
 	private Paint paint = new Paint();
-	
+	int lastX;
+	int lastY;
+
+	Drawable mSplitter;
+
 	public SplitPaneLayout(Context context) {
 		super(context);
 	}
-	
+
 	public SplitPaneLayout(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
-	
+
 	public SplitPaneLayout(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		if (attrs != null) {
-			TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SplitPaneLayout);
+			TypedArray a = context.obtainStyledAttributes(attrs,
+					R.styleable.SplitPaneLayout);
 			mOrientation = a.getInt(R.styleable.SplitPaneLayout_orientation, 0);
-			mSplitterSize = a.getDimensionPixelSize(R.styleable.SplitPaneLayout_splitterSize, 12);
-			
-			TypedValue value = a.peekValue(R.styleable.SplitPaneLayout_splitterPosition);
+			mSplitterSize = a.getDimensionPixelSize(
+					R.styleable.SplitPaneLayout_splitterSize, 12);
+
+			TypedValue value = a
+					.peekValue(R.styleable.SplitPaneLayout_splitterPosition);
 			if (value != null) {
 				if (value.type == TypedValue.TYPE_DIMENSION) {
-					mSplitterPositionPixel = a.getDimensionPixelSize(R.styleable.SplitPaneLayout_splitterPosition, 0);
+					mSplitterPositionPixel = a.getDimensionPixelSize(
+							R.styleable.SplitPaneLayout_splitterPosition, 0);
+				} else if (value.type == TypedValue.TYPE_FRACTION) {
+					mSplitterPositionPercent = Math.round(a.getFraction(
+							R.styleable.SplitPaneLayout_splitterPosition, 100,
+							100, 50));
 				}
-				else if (value.type == TypedValue.TYPE_FRACTION) {
-					mSplitterPositionPercent = Math.round(a.getFraction(R.styleable.SplitPaneLayout_splitterPosition, 100, 100, 50));
-				}
-			}
-			else {
+			} else {
 				mSplitterPositionPixel = 0;
-				mSplitterPositionPercent = 50;				
+				mSplitterPositionPercent = 50;
 			}
-			mSplitterBackgroundResource = a.getResourceId(R.styleable.SplitPaneLayout_splitterBackground, 0);
+			mSplitterBackgroundResource = a.getResourceId(
+					R.styleable.SplitPaneLayout_splitterBackground, 0);
+			if (mSplitterBackgroundResource != 0) {
+				mSplitter = context.getResources().getDrawable(
+						mSplitterBackgroundResource);
+			}
 			a.recycle();
 		}
-		
-		View view = new View(getContext());
-		view.setBackgroundResource(mSplitterBackgroundResource);
-		this.addView(view);
-		view .setOnTouchListener
-		(
-			new OnTouchListener()
-			{
-				int lastX;
-				int lastY;
-				
-				@Override
-				public boolean onTouch(View v, MotionEvent event)
-				{
-					int x = (int) event.getX();
-					int y = (int) event.getY();
-					
-					switch (event.getAction())
-					{
-						case MotionEvent.ACTION_DOWN:
-						{
-							isDragging = true;
-							v.getDrawingRect(temp);
-							temp.offset(v.getLeft(), v.getTop());
-							lastX = x;
-							lastY = y;
-							invalidate();
-							break;
-						}
-						case MotionEvent.ACTION_MOVE:
-						{
-							if (isDragging)
-							{
-								switch (mOrientation)
-								{
-									case 0:
-									{
-										temp.offset((x - lastX), 0);
-										break;
-									}
-									case 1:
-									{
-										temp.offset(0, (int)(y - lastY));
-										break;
-									}								
-								}
-								lastX = x;
-								lastY = y;
-								invalidate();
-							}
-							break;
-						}
-						case MotionEvent.ACTION_UP:
-						{
-							isDragging = false;
-							switch (mOrientation)
-							{
-								case 0:
-								{
-									mSplitterPositionPixel += event.getX() - mSplitterSize / 2;
-									break;
-								}
-								case 1:
-								{
-									mSplitterPositionPixel += event.getY() - mSplitterSize / 2;
-									break;
-								}								
-							}							
-							mSplitterPositionPercent = 0;
-							requestLayout();
-							break;
-						}							
-					}
-					return true;
-				}
-				
-			}
-		);
-		
 	}
-	
+
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -166,247 +101,208 @@ public class SplitPaneLayout extends ViewGroup {
 		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
 		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-		
-		switch(mOrientation)
-		{
-			case 0:
-			{
-				if (!mSplitterPositionConfigured)
-				{
-					if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
-					{
-						mSplitterPositionPixel = Math.round(widthSize * (mSplitterPositionPercent / 100.0f));
-						mSplitterPositionConfigured = true;
-					}
-					else
-					if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
-					{
-						mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) widthSize) * 100);
-						mSplitterPositionConfigured = true;
-					}
-					this.getChildAt(1).measure
-					(
-						MeasureSpec.makeMeasureSpec
-						(
-							mSplitterPositionPixel - (mSplitterSize / 2),
-							MeasureSpec.EXACTLY
-						)
-						,
-						MeasureSpec.makeMeasureSpec
-						(
-							heightSize,
-							MeasureSpec.EXACTLY
-						)
-					);
-					this.getChildAt(0).measure
-					(
-						MeasureSpec.makeMeasureSpec
-						(
-							mSplitterSize,
-							MeasureSpec.EXACTLY
-						)
-						,
-						MeasureSpec.makeMeasureSpec
-						(
-							heightSize,
-							MeasureSpec.EXACTLY
-						)
-					);					
-					this.getChildAt(2).measure
-					(
-						MeasureSpec.makeMeasureSpec
-						(
-							widthSize - mSplitterPositionPixel - (mSplitterSize / 2),
-							MeasureSpec.EXACTLY
-						)
-						,
-						MeasureSpec.makeMeasureSpec
-						(
-							heightSize,
-							MeasureSpec.EXACTLY
-						)
-					);					
-				}
-			}
-			case 1:
-			{
-				if (!mSplitterPositionConfigured)
-				{
-					if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
-					{
-						mSplitterPositionPixel = Math.round(heightSize * (mSplitterPositionPercent / 100.0f));
-						mSplitterPositionConfigured = true;
-					}
-					else
-					if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
-					{
-						mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) heightSize) * 100);
-						mSplitterPositionConfigured = true;
-					}
 
-					this.getChildAt(1).measure
-					(
-						MeasureSpec.makeMeasureSpec
-						(
-							widthSize,
-							MeasureSpec.EXACTLY
-						)
-						,
-						MeasureSpec.makeMeasureSpec
-						(
-
-							mSplitterPositionPixel - (mSplitterSize / 2),
-							MeasureSpec.EXACTLY							
-						)
-					);
-					this.getChildAt(0).measure
-					(
-						MeasureSpec.makeMeasureSpec
-						(
-							widthSize,
-							MeasureSpec.EXACTLY
-						)
-						,
-						MeasureSpec.makeMeasureSpec
-						(
-							mSplitterSize,
-							MeasureSpec.EXACTLY
-						)
-					);					
-					this.getChildAt(2).measure
-					(
-						MeasureSpec.makeMeasureSpec
-						(
-							heightSize,
-							MeasureSpec.EXACTLY
-						)
-						,
-						MeasureSpec.makeMeasureSpec
-						(
-							heightSize - mSplitterPositionPixel - (mSplitterSize / 2),
-							MeasureSpec.EXACTLY							
-						)
-					);					
+		switch (mOrientation) {
+		case 0: {
+			if (!mSplitterPositionConfigured) {
+				if (mSplitterPositionPixel == 0
+						&& mSplitterPositionPercent != 0) {
+					mSplitterPositionPixel = Math.round(widthSize
+							* (mSplitterPositionPercent / 100.0f));
+					mSplitterPositionConfigured = true;
+				} else if (mSplitterPositionPixel != 0
+						&& mSplitterPositionPercent == 0) {
+					mSplitterPositionPercent = Math
+							.round(((float) mSplitterPositionPixel / (float) widthSize) * 100);
+					mSplitterPositionConfigured = true;
 				}
+				this.getChildAt(0).measure(
+						MeasureSpec.makeMeasureSpec(mSplitterPositionPixel
+								- (mSplitterSize / 2), MeasureSpec.EXACTLY),
+						MeasureSpec.makeMeasureSpec(heightSize,
+								MeasureSpec.EXACTLY));
+
+				this.getChildAt(1).measure(
+						MeasureSpec.makeMeasureSpec(widthSize
+								- mSplitterPositionPixel - (mSplitterSize / 2),
+								MeasureSpec.EXACTLY),
+						MeasureSpec.makeMeasureSpec(heightSize,
+								MeasureSpec.EXACTLY));
 			}
-		}		
+		}
+		case 1: {
+			if (!mSplitterPositionConfigured) {
+				if (mSplitterPositionPixel == 0
+						&& mSplitterPositionPercent != 0) {
+					mSplitterPositionPixel = Math.round(heightSize
+							* (mSplitterPositionPercent / 100.0f));
+					mSplitterPositionConfigured = true;
+				} else if (mSplitterPositionPixel != 0
+						&& mSplitterPositionPercent == 0) {
+					mSplitterPositionPercent = Math
+							.round(((float) mSplitterPositionPixel / (float) heightSize) * 100);
+					mSplitterPositionConfigured = true;
+				}
+
+				this.getChildAt(0).measure(
+						MeasureSpec.makeMeasureSpec(widthSize,
+								MeasureSpec.EXACTLY),
+						MeasureSpec.makeMeasureSpec(
+
+						mSplitterPositionPixel - (mSplitterSize / 2),
+								MeasureSpec.EXACTLY));
+
+				this.getChildAt(1).measure(
+						MeasureSpec.makeMeasureSpec(heightSize,
+								MeasureSpec.EXACTLY),
+						MeasureSpec.makeMeasureSpec(heightSize
+								- mSplitterPositionPixel - (mSplitterSize / 2),
+								MeasureSpec.EXACTLY));
+			}
+		}
+		}
 	}
-	
+
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		int w = r - l;
 		int h = b - t;
-		
-		switch(mOrientation)
-		{
-			case 0:
-			{
-				if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
-				{
-					mSplitterPositionPixel = Math.round(w * (mSplitterPositionPercent / 100.0f));
-					mSplitterPositionConfigured = true;
-				}
-				else
-				if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
-				{
-					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) w) * 100);
-					mSplitterPositionConfigured = true;
-				}
-				else
-				{
-					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) w) * 100);
-					mSplitterPositionConfigured = true;
-				}
-				// We need to tell the child elements about their new dimensions;
-				this.getChildAt(1).measure
-				(
-					MeasureSpec.makeMeasureSpec
-					(
-						mSplitterPositionPixel - (mSplitterSize / 2) - l,
-						MeasureSpec.EXACTLY
-					)
-					,
-					MeasureSpec.makeMeasureSpec
-					(
 
-						h,
-						MeasureSpec.EXACTLY							
-					)
-				);					
-				this.getChildAt(1).layout
-				(
-					l, t, mSplitterPositionPixel - (mSplitterSize / 2), b
-				);
-				this.getChildAt(0).layout
-				(
-					mSplitterPositionPixel - (mSplitterSize / 2), t, mSplitterPositionPixel + (mSplitterSize / 2), b
-				);
-				// We need to tell the child elements about their new dimensions;
-				this.getChildAt(2).measure
-				(
-					MeasureSpec.makeMeasureSpec
-					(
-						r - (mSplitterPositionPixel + (mSplitterSize / 2)),
-						MeasureSpec.EXACTLY
-					)
-					,
-					MeasureSpec.makeMeasureSpec
-					(
+		switch (mOrientation) {
+		case 0: {
+			if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0) {
+				mSplitterPositionPixel = Math.round(w
+						* (mSplitterPositionPercent / 100.0f));
+				mSplitterPositionConfigured = true;
+			} else if (mSplitterPositionPixel != 0
+					&& mSplitterPositionPercent == 0) {
+				mSplitterPositionPercent = Math
+						.round(((float) mSplitterPositionPixel / (float) w) * 100);
+				mSplitterPositionConfigured = true;
+			} else {
+				mSplitterPositionPercent = Math
+						.round(((float) mSplitterPositionPixel / (float) w) * 100);
+				mSplitterPositionConfigured = true;
+			}
+			this.getChildAt(0).measure(
+					MeasureSpec.makeMeasureSpec(mSplitterPositionPixel
+							- (mSplitterSize / 2) - l, MeasureSpec.EXACTLY),
+					MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY));
+			this.getChildAt(0).layout(l, t,
+					mSplitterPositionPixel - (mSplitterSize / 2), b);
+			curr.set(mSplitterPositionPixel - (mSplitterSize / 2), t,
+					mSplitterPositionPixel + (mSplitterSize / 2), b);
+			this.getChildAt(1).measure(
+					MeasureSpec.makeMeasureSpec(r
+							- (mSplitterPositionPixel + (mSplitterSize / 2)),
+							MeasureSpec.EXACTLY),
+					MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY));
+			this.getChildAt(1).layout(
+					mSplitterPositionPixel + (mSplitterSize / 2), t, r, b);
+			break;
+		}
+		case 1: {
+			if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0) {
+				mSplitterPositionPixel = Math.round(h
+						* (mSplitterPositionPercent / 100.0f));
+				mSplitterPositionConfigured = true;
+			} else if (mSplitterPositionPixel != 0
+					&& mSplitterPositionPercent == 0) {
+				mSplitterPositionPercent = Math
+						.round(((float) mSplitterPositionPixel / (float) h) * 100);
+				mSplitterPositionConfigured = true;
+			} else {
+				mSplitterPositionPercent = Math
+						.round(((float) mSplitterPositionPixel / (float) h) * 100);
+				mSplitterPositionConfigured = true;
+			}
 
-						h,
-						MeasureSpec.EXACTLY							
-					)
-				);						
-				this.getChildAt(2).layout
-				(
-					mSplitterPositionPixel + (mSplitterSize / 2), t, r, b
-				);		
-				break;
-			}
-			case 1:
-			{
-				if (mSplitterPositionPixel == 0 && mSplitterPositionPercent != 0)
-				{
-					mSplitterPositionPixel = Math.round(h * (mSplitterPositionPercent / 100.0f));
-					mSplitterPositionConfigured = true;
-				}
-				else
-				if (mSplitterPositionPixel != 0 && mSplitterPositionPercent == 0)
-				{
-					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) h) * 100);
-					mSplitterPositionConfigured = true;
-				}
-				else
-				{
-					mSplitterPositionPercent = Math.round(((float) mSplitterPositionPixel / (float) h) * 100);
-					mSplitterPositionConfigured = true;
-				}
-				
-				this.getChildAt(1).layout
-				(
-					l, t, r, mSplitterPositionPixel - (mSplitterSize / 2)
-				);
-				this.getChildAt(0).layout
-				(
-					l, mSplitterPositionPixel - (mSplitterSize / 2), r, mSplitterPositionPixel + (mSplitterSize / 2)
-				);					
-				this.getChildAt(2).layout
-				(
-					l, mSplitterPositionPixel + (mSplitterSize / 2), r, b
-				);						
-				break;
-			}
+			this.getChildAt(0).layout(l, t, r,
+					mSplitterPositionPixel - (mSplitterSize / 2));
+			curr.set(l, mSplitterPositionPixel - (mSplitterSize / 2), r,
+					mSplitterPositionPixel + (mSplitterSize / 2));
+			this.getChildAt(1).layout(l,
+					mSplitterPositionPixel + (mSplitterSize / 2), r, b);
+			break;
+		}
 		}
 	}
 
 	@Override
 	protected void dispatchDraw(Canvas canvas) {
 		super.dispatchDraw(canvas);
-		if (isDragging)
-		{
-			paint.setColor(Color.argb(128, 255,255,255));
-			canvas.drawRect(temp, paint);			
-		}		
+		mSplitter.setBounds(curr);
+		mSplitter.draw(canvas);
+		if (isDragging) {
+			paint.setColor(Color.argb(128, 255, 255, 255));
+			canvas.drawRect(temp, paint);
+		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int x = (int) event.getX();
+		int y = (int) event.getY();
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN: {
+			switch (mOrientation) {
+			case 0: {
+				temp.set(curr);
+				break;
+			}
+			case 1: {
+				temp.set(curr);
+				break;
+			}
+			}
+			if (temp.contains(x, y)) {
+				isDragging = true;
+			}
+			lastX = x;
+			lastY = y;
+			invalidate();
+			break;
+		}
+		case MotionEvent.ACTION_MOVE: {
+			if (isDragging) {
+				switch (mOrientation) {
+				case 0: {
+					temp.offset((x - lastX), 0);
+					break;
+				}
+				case 1: {
+					temp.offset(0, (int) (y - lastY));
+					break;
+				}
+				}
+				lastX = x;
+				lastY = y;
+				invalidate();
+			}
+			break;
+		}
+		case MotionEvent.ACTION_UP: {
+			if (isDragging) {
+				isDragging = false;
+				switch (mOrientation) {
+				case 0: {
+					mSplitterPositionPixel = temp.left;
+					break;
+				}
+				case 1: {
+					mSplitterPositionPixel = temp.top;
+					break;
+				}
+				}
+				mSplitterPositionPercent = 0;
+				requestLayout();
+				invalidate();
+			}
+			break;
+		}
+		}
+		return true;
 	}
 
 }
